3157fb26a553253fd63e41c5f0cdb23ba6ffceaf
Myriam Boran <brnmyriam45@googlemail.com>
2019-05-25 12:55:41 +0100

    homework

---
+++ client/src/views/select_view.js
@@ -0,0 +1,28 @@
+import {PubSub} from '../helpers/pub_sub.js';
+
+ export default class SelectView{
+   constructor(element){
+     this.element = element;
+   }
+
+   populate(instrumentFamiliesData){
+     instrumentFamiliesData.forEach((family, index) => {
+       const option = document.createElement('option');
+       option.textContent = family.name;
+       option.value = index;
+       this.element.appendChild(option);
+      });
+   };
+
+   bindEvents(){
+     PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+       const allInstrumentFamilies = evt.detail;
+       this.populate(allInstrumentFamilies);
+     });
+
+     this.element.addEventListener('change', (evt) => {
+       const selectedIndex = evt.target.value;
+       PubSub.publish('SelectView:change', selectedIndex);
+     });
+   };
+ };
